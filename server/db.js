772268ac850e546f11aa056938b1be298bd9e9f2
mheviag <42681767+mheviag@users.noreply.github.com>
2020-11-28 22:56:00 -0700

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,11 +1,11 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-    user: "idqprqdiimjuxi",
-    password: "3f4a0adda50d8ef588931f0e49c7e2163242368fbf49e048c9e36f3df4d6f62d",
-    host: "ec2-54-196-89-124.compute-1.amazonaws.com",
+    user: "postgres",
+    password: "",
+    host: "localhost",
     port: 5432,
-    database: "d51927giuf9dfq"
+    database: "wetbat"
 });
 
 module.exports = pool;
